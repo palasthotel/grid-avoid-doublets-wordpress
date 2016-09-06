405cdc4eb7dbd225e4afe398c764585935bffc3a
EdwardBock <edward.bock@palasthotel.de>
2016-09-06 18:15:26 +0200

    general mechanismn

--- grid_avoid_doubletts.php
+++ grid_avoid_doubletts.php
@@ -12,9 +12,106 @@
  * @package Palasthotel\Grid-WordPress-Box-Social
  */
 
-class GridAvoidDoubletts {
+class GridAvoidDoubletts{
+	
+	/**
+	 * array key for not grid specific post ids
+	 */
+	const GLOBAL_KEY = "__global__";
+	
+	/**
+	 * @var array
+	 */
+	private $areas;
+	
+	/**
+	 * GridAvoidDoubletts constructor.
+	 */
 	public function __construct() {
+		$this->areas = array();
+		
+		// TODO: on action start render clear
 		
 	}
+	
+	/**
+	 * add an placed constents for example and post id in a grid id
+	 *
+	 * @param integer $content_id unique for area
+	 * @param null|integer|string $area_id id for area
+	 *
+	 */
+	public function addContentId( $content_id, $area_id = null){
+		/**
+		 * if no grid id set to global index
+		 */
+		if( $area_id == null) $area_id = self::GLOBAL_KEY;
+		
+		/**
+		 * create new entry for grid id
+		 */
+		if(!isset($this->areas[$area_id])){
+			$this->areas[$area_id] = array();
+		}
+		
+		/**
+		 * add post id if not already set
+		 */
+		if(!in_array( $content_id, $this->areas[$area_id])){
+			$this->areas[$area_id][] = $content_id;
+		}
+	}
+	
+	/**
+	 * check if post is already placed
+	 * @param integer $post_id
+	 * @param self::GLOBAL_KEY|integer|null $grid_id
+	 *
+	 * @return bool
+	 */
+	public function isPlaced($post_id, $grid_id = null){
+		if($grid_id != null){
+			/**
+			 * have a look in the specific grid
+			 */
+			return ( isset($this->areas[$grid_id]) && in_array($post_id, $this->areas[$grid_id]));
+		}
+		
+		/**
+		 * have a look on all placed posts in all grids
+		 */
+		foreach ($this->areas as $_grid_id => $_post_id){
+			if($_post_id == $post_id) return true;
+		}
+		
+		/**
+		 * if nothing found its not placed
+		 */
+		return false;
+	}
+	
 }
-new GridAvoidDoubletts();
+
+/**
+ * initialize
+ */
+global $grid_avoid_doubletts;
+$grid_avoid_doubletts = new GridAvoidDoubletts();
+
+/**
+ * @param integer $post_id
+ * @param string | integer $grid_id
+ */
+function grid_avoid_doubletts_add($post_id, $grid_id = "global"){
+	global $grid_avoid_doubletts;
+	$grid_avoid_doubletts->addContentId($post_id, $grid_id);
+}
+
+/**
+ * @param integer $post_id
+ * @param null | string | integer  $grid_id
+ */
+function grid_avoid_doubletts_is_placed($post_id, $grid_id = null){
+	global $grid_avoid_doubletts;
+	$grid_avoid_doubletts->isPlaced($post_id, $grid_id);
+}
